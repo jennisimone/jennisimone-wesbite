4737f4b56c55b0ae7982f8aa709837def90b8632
Jenni Foster <jennisimone@gmail.com>
2023-09-14 19:59:04 +0100

    Update website homepage

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -5,31 +5,56 @@ interface HomeProps {
 }
 
 const Home: FC<HomeProps> = () => (
-    <div className={styles.Home}>
-        <div className={styles.Panel}>
-            <img className={styles.MobileImage} alt='game screenshots' src={'images/mobile-banner.png'}/>
-            <img className={styles.DesktopImage} alt='game screenshots' src={'images/banner.png'}/>
-            <div className={styles.Title}>
-                <div>
-                    Free games and apps for everyone to enjoy.
-                </div>
-                <div className={styles.Technologies}>
-                    <div className={styles.TechUsed}>Technologies</div>
-                    <div className={styles.TechIcons}>
-                        <img className={styles.TechImage} alt='Android Studio' src={'images/icons/technologies/Android_Studio_Icon.png'}/>
-                        <img className={styles.TechImage} alt='Unity' src={'images/icons/technologies/Unity_Icon.png'}/>
-                        <img className={styles.TechImage} alt='Phaser' src={'images/icons/technologies/Phaser_Icon.png'}/>
+        <div className={styles.Home}>
+            <div className={styles.Panel}>
+                <div className={styles.HomeLogo}>JENNISIMONE</div>
+                <div className={styles.Skills}>
+                    <div className={styles.SkillsContainer}>
+                        <img className={styles.SkillsImage} alt={'Itch.io game page'} src={'images/Games.png'}></img>
+                        <div className={styles.SkillsName}>Games</div>
                     </div>
-                    <div className={styles.TechUsed}>Languages</div>
-                    <div className={styles.TechIcons}>
-                        <img className={styles.TechImage} alt='C#' src={'images/icons/technologies/CSharp_Icon.png'}/>
-                        <img className={styles.TechImage} alt='Kotlin' src={'images/icons/technologies/Kotlin_Icon.png'}/>
-                        <img className={styles.TechImage} alt='JavaScript' src={'images/icons/technologies/Javascript_Icon.png'}/>
+                    <div className={styles.SkillsContainer}>
+                        <img className={styles.SkillsImage} alt={'Some code'} src={'images/Programming.png'}></img>
+                        <div className={styles.SkillsName}>Programming</div>
+                    </div>
+                    <div className={styles.SkillsContainer}>
+                        <img className={styles.SkillsImage} alt={'Some blog notes'} src={'images/Blogging.png'}></img>
+                        <div className={styles.SkillsName}>Blogging</div>
+                    </div>
+                </div>
+                <div className={styles.Title}>
+                    <div className={styles.Technologies}>
+                        <div className={styles.TechnologiesSection}>
+                            <div className={styles.TechUsed}>Technologies</div>
+                            <div className={styles.TechIcons}>
+                                <img className={styles.TechImage} alt='Android Studio'
+                                     src={'images/icons/technologies/Android_Studio_Icon.png'}/>
+                                <img className={styles.TechImage} alt='Unity'
+                                     src={'images/icons/technologies/Unity_Icon.png'}/>
+                                <img className={styles.TechImage} alt='Phaser'
+                                     src={'images/icons/technologies/Phaser_Icon.png'}/>
+                                <img className={styles.TechImage} alt='Game Maker Studio'
+                                     src={'images/icons/technologies/Phaser_Icon.png'}/>
+                            </div>
+                        </div>
+                        <div className={styles.TechnologiesSection}>
+                            <div className={styles.TechUsed}>Languages</div>
+                            <div className={styles.TechIcons}>
+                                <img className={styles.TechImage} alt='C#'
+                                     src={'images/icons/technologies/CSharp_Icon.png'}/>
+                                <img className={styles.TechImage} alt='Kotlin'
+                                     src={'images/icons/technologies/Kotlin_Icon.png'}/>
+                                <img className={styles.TechImage} alt='JavaScript'
+                                     src={'images/icons/technologies/Javascript_Icon.png'}/>
+                                <img className={styles.TechImage} alt='Java'
+                                     src={'images/icons/technologies/Javascript_Icon.png'}/>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-);
+    )
+;
 
 export default Home;
