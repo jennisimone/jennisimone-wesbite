cdc3323cd665184f2891e6d11adee5107118ab71
Jenni Foster <jennisimone@gmail.com>
2023-09-14 22:44:38 +0100

    Update incorrect logos

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -27,27 +27,27 @@ const Home: FC<HomeProps> = () => (
                         <div className={styles.TechnologiesSection}>
                             <div className={styles.TechUsed}>Technologies</div>
                             <div className={styles.TechIcons}>
-                                <img className={styles.TechImage} alt='Android Studio'
+                                <img className={styles.TechImage} alt='Android Studio' title='Android Studio'
                                      src={'images/icons/technologies/Android_Studio_Icon.png'}/>
-                                <img className={styles.TechImage} alt='Unity'
+                                <img className={styles.TechImage} alt='Unity' title='Unity'
                                      src={'images/icons/technologies/Unity_Icon.png'}/>
-                                <img className={styles.TechImage} alt='Phaser'
+                                <img className={styles.TechImage} alt='Phaser' title='Phaser'
                                      src={'images/icons/technologies/Phaser_Icon.png'}/>
-                                <img className={styles.TechImage} alt='Game Maker Studio'
-                                     src={'images/icons/technologies/Phaser_Icon.png'}/>
+                                <img className={styles.TechImage} alt='Game Maker Studio' title='Game Maker Studio'
+                                     src={'images/icons/technologies/GameMaker_Icon.png'}/>
                             </div>
                         </div>
                         <div className={styles.TechnologiesSection}>
                             <div className={styles.TechUsed}>Languages</div>
                             <div className={styles.TechIcons}>
-                                <img className={styles.TechImage} alt='C#'
+                                <img className={styles.TechImage} alt='C#' title='C#'
                                      src={'images/icons/technologies/CSharp_Icon.png'}/>
-                                <img className={styles.TechImage} alt='Kotlin'
+                                <img className={styles.TechImage} alt='Kotlin' title='Kotlin'
                                      src={'images/icons/technologies/Kotlin_Icon.png'}/>
-                                <img className={styles.TechImage} alt='JavaScript'
+                                <img className={styles.TechImage} alt='JavaScript' title='JavaScript'
                                      src={'images/icons/technologies/Javascript_Icon.png'}/>
-                                <img className={styles.TechImage} alt='Java'
-                                     src={'images/icons/technologies/Javascript_Icon.png'}/>
+                                <img className={styles.TechImage} alt='Java' title='Java'
+                                     src={'images/icons/technologies/Java_Icon.png'}/>
                             </div>
                         </div>
                     </div>
